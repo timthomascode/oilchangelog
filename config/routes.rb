04a67b3383e2ddb09075361ce12712fdf963d307
timthomasdev <tim.thomas@hey.com>
2019-01-01 10:13:13 -0600

    Nest oil_changes route inside vehicles route

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  resources :oil_changes
-  resources :vehicles
+  resources :vehicles do
+    resource :oil_changes
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end

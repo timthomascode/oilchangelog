2b242961804e1583b4f2831812d0d5ab559f761b
timthomasdev <tim.thomas@hey.com>
2019-01-04 14:13:37 -0600

    Remove index and show routes for oil changes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   resources :vehicles do
-    resources :oil_changes
+    resources :oil_changes, only: [:new, :create, :edit, :update, :destroy]
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end

e282c8a5ebe508be006962b6e56dcc280be7b7e5
timthomasdev <tim.thomas@hey.com>
2019-01-22 17:10:58 -0600

    Redirect to to vehicles index when a new vehicle is saved

--- app/controllers/vehicles_controller.rb
+++ app/controllers/vehicles_controller.rb
@@ -29,7 +29,7 @@ class VehiclesController < ApplicationController
 
     respond_to do |format|
       if @vehicle.save
-        format.html { redirect_to @vehicle, notice: 'Vehicle was successfully created.' }
+        format.html { redirect_to vehicles_path, notice: 'Vehicle was successfully created.' }
         format.json { render :show, status: :created, location: @vehicle }
       else
         format.html { render :new }

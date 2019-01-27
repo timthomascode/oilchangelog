386775340b601d39cc7fea42eb5ed4b29ae8a3e1
timthomasdev <tim.thomas@hey.com>
2019-01-27 10:55:12 -0600

    Add seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.delete_all
+#Vehicle.delete_all
+#OilChange.delete_all
+
+test_user = User.create!(email: "test@test.com", password: "password")
+test2_user = User.create!(email: "test2@test.com", password: "password")
+
+test_users_car = Vehicle.create!(user: test_user, nickname: "test's car", make: "test", model: "tester", mileage: 100)
+test_users_car2 = Vehicle.create!(user: test_user, nickname: "test's second car", make: "test", model: "tester", mileage: 200)
+
+test2_users_car = Vehicle.create!(user: test2_user, nickname: "test 2's car", make: "test", model: "tester", mileage: 100)
+test2_users_car2 = Vehicle.create!(user: test2_user, nickname: "test 2's second car", make: "test", model: "tester", mileage: 200)
+
+OilChange.create!(vehicle: test_users_car, date: Date.today, mileage: 50, oil_used: "castrol gtx 9000", notes: "metal shavings in oil")

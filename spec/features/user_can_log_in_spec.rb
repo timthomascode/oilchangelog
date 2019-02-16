c911c5fa757d6eee9c28288dd3bb3b9930e64bf2
timthomasdev <tim.thomas@hey.com>
2019-02-16 10:09:53 -0600

    Add user login integration test

---
+++ spec/features/user_can_log_in_spec.rb
@@ -0,0 +1,13 @@
+require 'rails_helper'
+
+feature 'User can log in' do
+  scenario "when using correct credentials" do
+    User.create(email: "someone@something.com", password: "password")
+    visit root_path
+    fill_in "Email",	with: "someone@something.com" 
+    fill_in "Password",	with: "password" 
+    click_on "Sign in"
+
+    expect(page).to have_content 'Vehicles'
+  end
+end

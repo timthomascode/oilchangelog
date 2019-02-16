e08dea052a29280c4c75a0a78dfa114fa66b3c7f
timthomasdev <tim.thomas@hey.com>
2019-02-16 10:09:43 -0600

    Add user creation integration test

---
+++ spec/features/user_can_create_an_account_spec.rb
@@ -0,0 +1,13 @@
+require 'rails_helper'
+
+feature "User can create an account" do
+  scenario "with email and password" do
+    visit root_path
+    click_on "Sign up"
+    fill_in "Email",	with: "someone@something.com" 
+    fill_in "Password",	with: "password" 
+    click_on "Sign up"
+
+    expect(page).to have_content 'Signed in as: someone@something.com'
+  end
+end

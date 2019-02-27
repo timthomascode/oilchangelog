411006f1dfe3a6a16160b3faaa376390473487de
timthomasdev <tim.thomas@hey.com>
2019-02-27 12:58:51 -0600

    Add validations and tests for oil changes

--- app/models/oil_change.rb
+++ app/models/oil_change.rb
@@ -1,3 +1,7 @@
 class OilChange < ApplicationRecord
   belongs_to :vehicle
+
+  validates :date, presence: true
+  validates :mileage, presence: true
+  validates :oil_used, presence: true
 end
--- spec/models/oil_change_spec.rb
+++ spec/models/oil_change_spec.rb
@@ -1,5 +1,22 @@
 require 'rails_helper'
 
 RSpec.describe OilChange, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  # OilChange.new(date: Date.today, mileage: 100, oil_used: "Some Oil", notes: "Some Notes", vehicle: test_vehicle)
+  let (:test_user) { User.new(email: "test@testing.com", password: "password") }
+  let (:test_vehicle) {Vehicle.new(nickname: "Test Car", make: "Honda", model: "Civic", mileage: 0, user: test_user)}
+
+  it "is invalid without a date" do
+    oil_change = OilChange.new(mileage: 100, oil_used: "Some Oil", notes: "Some Notes", vehicle: test_vehicle)
+    expect(oil_change).to_not be_valid   
+  end
+
+  it "is invalid without a mileage" do
+    oil_change = OilChange.new(date: Date.today, oil_used: "Some Oil", notes: "Some Notes", vehicle: test_vehicle)
+    expect(oil_change).to_not be_valid  
+  end
+
+  it "is invalid without an oil used" do
+    oil_change = OilChange.new(date: Date.today, mileage: 100, notes: "Some Notes", vehicle: test_vehicle)
+    expect(oil_change).to_not be_valid  
+  end
 end

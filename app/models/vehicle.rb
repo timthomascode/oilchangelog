a94cc6cc523e1aa9432ba74c480d370d2ac040a1
timthomasdev <tim.thomas@hey.com>
2019-02-27 12:33:14 -0600

    Add validations and tests for vehicle

--- app/models/vehicle.rb
+++ app/models/vehicle.rb
@@ -1,4 +1,9 @@
 class Vehicle < ApplicationRecord
   has_many :oil_changes, dependent: :destroy
   belongs_to :user
+
+  validates :nickname, presence: true
+  validates :make, presence: true
+  validates :model, presence: true
+  validates :mileage, presence: true
 end
--- spec/models/vehicle_spec.rb
+++ spec/models/vehicle_spec.rb
@@ -1,5 +1,30 @@
 require 'rails_helper'
 
 RSpec.describe Vehicle, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let (:test_user) { User.new(email: "test@testing.com", password: "password") }
+
+  it "is invalid without a make" do
+    vehicle = Vehicle.new(nickname: "Test Car", model: "Civic", mileage: 0, user: test_user)
+    expect(vehicle).to_not be_valid
+  end
+
+  it "is invalid without a nickname" do
+    vehicle = Vehicle.new(make: "Honda", model: "Civic", mileage: 0, user: test_user)
+    expect(vehicle).to_not be_valid
+  end
+
+  it "is invalid without a model" do
+    vehicle = Vehicle.new(nickname: "Test Car", make: "Honda", mileage: 0, user: test_user)
+    expect(vehicle).to_not be_valid
+  end
+
+  it "is invalid without a user" do
+    vehicle = Vehicle.new(nickname: "Test Car", make: "Honda", model: "Civic", mileage: 0)
+    expect(vehicle).to_not be_valid
+  end
+
+  it "is invalid without a mileage" do
+    vehicle = Vehicle.new(nickname: "Test Car", make: "Honda", model: "Civic", user: test_user)
+    expect(vehicle).to_not be_valid
+  end
 end
